1b7481eb7a0a6348becf3ede3b4b065950deba41
Steve <spdevuk@gmail.com>
2018-06-07 11:14:33 +0100

    added delete route and if / else statements to secure it

--- routes/api/settings/todos.js
+++ routes/api/settings/todos.js
@@ -16,6 +16,19 @@ const TodosModel = require('../../../models/Todos.js');
 const { sanitizeBody } = require('express-validator/filter');
 const validateTodoInput = require('../../../validation/todos');
 
+// @route GET api/users/:user/todos
+// @desc list all of the current todos for a logged in user
+// @access Private
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+  // find what user is opening the page
+  UserModel.findOne({ user: req.user.id }).then(user => {
+    if (req.user) {
+      console.log(req.user);
+    }
+    TodosModel.find({ user: req.user.id }).then(todos => res.json(todos));
+  });
+});
+
 // @route POST api/users/:user/todos
 // @desc create a new todo for a logged in user
 // @access Private
@@ -45,14 +58,23 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
   });
 });
 
-// @route GET api/users/:user/todos
-// @desc list all of the current todos for a logged in user
+// @route DELETE api/users/:user/todos/:todo_id
+// @desc delete a specific todo
 // @access Private
-router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
-  // find what user is opening the page
-  UserModel.findOne({ user: req.user.id }).then(user => {
-    TodosModel.find({ user: req.user.id }).then(posts => res.json(posts));
-  });
+router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  UserModel.findOne({ user: req.user.id })
+    .then(user => {
+      TodosModel.findById(req.params.id).then(todo => {
+        // if the user.id making the request is not the same as the jWT.id stop
+        if (todo.user.toString() !== req.user.id.toString()) {
+          return res.status(401).json({ notauthorized: 'User is not authoritzed ' });
+        }
+        // if the user.id and the JWT.id are the same then the user owns this todo - delete it
+        todo.remove().then(() => res.json({ success: true }));
+      });
+    })
+    .catch(err => res.status(404).json({ nopostfound: 'No todo was found with that ID' }));
 });
 
+// TODO: add update & edit with front-end using parms
 module.exports = router;
