9e48df0f82af8ef123a8ed9843ee9ce77c801cce
Steve <spdevuk@gmail.com>
2018-06-10 21:04:40 +0100

    added layout for clock

--- client/src/components/Clock/Clock.jsx
+++ client/src/components/Clock/Clock.jsx
@@ -15,32 +15,55 @@ import Searchbar from './Searchbar/Searchbar';
 
 // send the user with this request
 const styles = {
-  clock: {
+  wrapper: {
     position: 'fixed',
     top: '50%',
     left: '50%',
     height: 400,
     width: 500,
-    backgroundColor: 'red',
+    border: '2px solid red',
     transform: 'translate(-50%, -50%)',
     display: 'flex',
     flexDirection: 'column'
+  },
+  // when changing 24 hr etc change the padding to make it fit.
+  clockwrapper: {
+    padding: '0 19%'
+  },
+  clock: {
+    fontSize: '5em',
+    color: '#FAFAFA',
+    fontWeight: 400,
+    fontFamily: 'Roboto',
+    textAlign: 'center',
+    opacity: '0.9',
+    textShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
+    marginBottom: 0
+  },
+  date: {
+    fontSize: '1.02em',
+    color: '#FAFAFA',
+    fontFamily: 'Roboto',
+    marginTop: -10,
+    fontWeight: 300,
+    paddingLeft: 20,
+    opacity: '0.9'
   }
 };
 
 @inject('authStore')
 @observer
 class Clock extends Component {
-  render() {
-    const { authStore } = this.props;
+  render(props) {
+    const { classes, authStore } = this.props;
     // fetches current user clock settings from DB when they are authenticated
     if (authStore.isAuthenticated) authStore.setClock();
 
     return (
-      <div className="clock">
-        <div className="clock-display">
-          <h2>6:35PM</h2>
-          <h4>Monday June 4th 2018</h4>
+      <div className={classes.wrapper}>
+        <div className={classes.clockwrapper}>
+          <h1 className={classes.clock}>2:35PM</h1>
+          <h4 className={classes.date}>Monday June 4th 2018</h4>
         </div>
         <Searchbar />
         <Bookmarks />
@@ -49,4 +72,4 @@ class Clock extends Component {
   }
 }
 
-export default Clock;
+export default withStyles(styles)(Clock);
