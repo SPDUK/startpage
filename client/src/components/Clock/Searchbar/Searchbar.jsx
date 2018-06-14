a981729f7d3ab25b660bc81bddcd15cc701e0618
SP <spdevuk@gmail.com>
2018-06-14 02:43:12 +0100

    added search options & function(s) to change the search

--- client/src/components/Clock/Searchbar/Searchbar.jsx
+++ client/src/components/Clock/Searchbar/Searchbar.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 import './SearchBar.scss';
 
 class SearchBar extends Component {
   constructor() {
     super();
     this.state = {
-      googleSearch: ''
+      searchInput: '',
+      anchorEl: null,
+      searchType: 'google'
     };
   }
   onChange = e => {
@@ -15,15 +19,39 @@ class SearchBar extends Component {
 
   onSubmit = e => {
     e.preventDefault();
-    const googleURL = `https://www.google.com/search?q=${this.state.googleSearch}`;
-    window.location.href = googleURL;
+    if (this.state.searchType === 'google') {
+      window.location.href = `https://www.google.com/search?q=${this.state.searchInput}`;
+    }
+    if (this.state.searchType === 'wikipedia') {
+      window.location.href = `https://en.wikipedia.org/wiki/${this.state.searchInput}`;
+    }
+    if (this.state.searchType === 'reddit') {
+      window.location.href = `https://www.reddit.com/search?q=${this.state.searchInput}`;
+    }
   };
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleToggle = () => {
+    this.setState({ open: !this.state.open });
+  };
+
+  handleClose = e => {
+    console.log(e.target);
+    this.setState({ searchType: e.target.id });
+    this.setState({ anchorEl: null });
+  };
+
   render() {
+    const { open } = this.state;
+    const { anchorEl } = this.state;
     return (
       <div className="searchbar">
         <form onSubmit={this.onSubmit}>
           <input
-            name="googleSearch"
+            name="searchInput"
             onSubmit={this.onSubmit}
             placeholder="Google"
             onChange={this.onChange}
@@ -31,7 +59,32 @@ class SearchBar extends Component {
             type="text"
           />
         </form>
-        <i className="material-icons searchbar-changer">arrow_drop_down </i>
+
+        <i
+          onClick={this.handleClick}
+          onKeyDown={this.handleClick}
+          className="material-icons searchbar-changer"
+          role="menu"
+          tabIndex={0}
+        >
+          arrow_drop_down{' '}
+        </i>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={this.handleClose}
+        >
+          <MenuItem id="google" onClick={this.handleClose}>
+            Google
+          </MenuItem>
+          <MenuItem id="wikipedia" onClick={this.handleClose}>
+            Wikipedia
+          </MenuItem>
+          <MenuItem id="reddit" onClick={this.handleClose}>
+            Reddit
+          </MenuItem>
+        </Menu>
       </div>
     );
   }
